452efdf072f64ebf2640c37fc512f881f9eaea8d
Daniel Rodriguez <sadasant@gmail.com>
2013-08-26 17:26:57 -0500

    Improved SquareMatrix, removed From and .Items
    SquareMatrix now is an almost-monadic matrix

--- NonMonads/SquareMatrix.go
+++ NonMonads/SquareMatrix.go
@@ -4,19 +4,10 @@ import (
 	"errors"
 )
 
-type Square [][]interface{}
-type SquareMatrix struct {
-	Items Square
-}
+type SquareMatrix [][]interface{}
 
-func (s *SquareMatrix) From(square Square) (SquareMatrix, error) {
-	s.Items = square
-	err := s.isWellFormed()
-	return *s, err
-}
-
-func (s SquareMatrix) isWellFormed() error {
-	if len(s.Items) > 0 && len(s.Items[0]) != len(s.Items[1]) {
+func (s SquareMatrix) IsOk() error {
+	if len(s[0]) != len(s[1]) {
 		return errors.New("The SquareMatrix is not Well Formed")
 	}
 	return nil
@@ -24,20 +15,19 @@ func (s SquareMatrix) isWellFormed() error {
 
 // Returns a square matrix containing a single value: the given value.
 func (s SquareMatrix) Wrap(v interface{}) (r SquareMatrix) {
-	r.Items = Square{[]interface{}{v}}
-	return
+	return SquareMatrix{[]interface{}{v}}
 }
 
 // Returns a square matrix created by running all of the items in the
 // given square matrix through a transformation function.
-func (s SquareMatrix) Transform(t func(interface{}) interface{}) (SquareMatrix, error) {
-	items := make(Square, len(s.Items[0]))
-	for i := 0; i < len(s.Items); i++ {
-		for j := 0; j < len(s.Items[i]); j++ {
-			items[i] = append(items[i], t(s.Items[i][j]))
+func (s SquareMatrix) Transform(t func(interface{}) interface{}) SquareMatrix {
+	r := make(SquareMatrix, len(s[0]))
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s[i]); j++ {
+			r[i] = append(r[i], t(s[i][j]))
 		}
 	}
-	return new(SquareMatrix).From(items)
+	return r
 }
 
 // Returns a square matrix containing the items inside the square
@@ -47,23 +37,23 @@ func (s SquareMatrix) Transform(t func(interface{}) interface{}) (SquareMatrix, 
 //
 // BROKEN - When inner matrices are of different sizes, there may not be
 // a square number of items.
-func (s SquareMatrix) Flatten() (SquareMatrix, error) {
-	switch len(s.Items) {
+func (s SquareMatrix) Flatten() SquareMatrix {
+	switch len(s) {
 	case 0:
-		return s, nil
+		return s
 	case 1:
-		if s0, ok := s.Items[0][0].(SquareMatrix); ok {
-			return s0, nil
+		if s0, ok := s[0][0].(SquareMatrix); ok {
+			return s0
 		}
-		return s, nil
+		return s
 	}
-	r := Square{}
-	for si := 0; si < len(s.Items); si++ {
-		for sj := 0; sj < len(s.Items[si]); sj++ {
-			if s0, ok := s.Items[si][sj].(SquareMatrix); ok {
-				innerSpan := len(s0.Items)
-				for s0i := 0; s0i < len(s0.Items); s0i++ {
-					for s0j := 0; s0j < len(s0.Items[s0i]); s0j++ {
+	r := SquareMatrix{}
+	for si := 0; si < len(s); si++ {
+		for sj := 0; sj < len(s[si]); sj++ {
+			if s0, ok := s[si][sj].(SquareMatrix); ok {
+				innerSpan := len(s0)
+				for s0i := 0; s0i < len(s0); s0i++ {
+					for s0j := 0; s0j < len(s0[s0i]); s0j++ {
 						i := si*innerSpan + s0i
 						if len(r) <= i {
 							r = append(r, []interface{}{})
@@ -72,13 +62,13 @@ func (s SquareMatrix) Flatten() (SquareMatrix, error) {
 						if len(r[i]) <= j {
 							r[i] = append(r[i], []interface{}{})
 						}
-						r[i][j] = s0.Items[s0i][s0j]
+						r[i][j] = s0[s0i][s0j]
 					}
 				}
 			} else {
-				r[si] = append(r[si], s.Items[si][sj])
+				r[si] = append(r[si], s[si][sj])
 			}
 		}
 	}
-	return new(SquareMatrix).From(r)
+	return r
 }
--- NonMonads/SquareMatrix_test.go
+++ NonMonads/SquareMatrix_test.go
@@ -7,75 +7,72 @@ import (
 
 func Test_SquareMatrix_Wrap(t *testing.T) {
 	s := new(SquareMatrix).Wrap("test")
-	if len(s.Items) != 1 {
-		t.Errorf("len(s)\nexpeted:%s\nreceived:%s", 1, len(s.Items))
+	if len(s) != 1 {
+		t.Errorf("len(s)\nexpeted:%s\nreceived:%s", 1, len(s))
 	}
-	if s.Items[0][0] != "test" {
-		t.Errorf("s[0][0]\nexpeted:%s\nreceived:%s", "test", s.Items[0][0])
+	if s[0][0] != "test" {
+		t.Errorf("s[0][0]\nexpeted:%s\nreceived:%s", "test", s[0][0])
 	}
 }
 
 func Test_SquareMatrix_Transform(t *testing.T) {
-	var s, expected SquareMatrix
-	s.From(Square{[]interface{}{3, 7}, []interface{}{2, 5}})
-	s, _ = s.Transform(func(v interface{}) interface{} {
+	r := SquareMatrix{[]interface{}{3, 7}, []interface{}{2, 5}}
+	s := r.Transform(func(v interface{}) interface{} {
 		i := v.(int)
 		return i * i
 	})
-	expected.From(Square{[]interface{}{9, 49}, []interface{}{4, 25}})
+	expected := SquareMatrix{[]interface{}{9, 49}, []interface{}{4, 25}}
 	if reflect.DeepEqual(s, expected) != true {
 		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, s)
 	}
 }
 
 func Test_SquareMatrix_Flatten(t *testing.T) {
-	var s1, s2, r3, s3, r4, s4, s5 SquareMatrix
 	var expected SquareMatrix
-	s1, _ = new(SquareMatrix).Wrap(new(SquareMatrix).Wrap("test")).Flatten()
-	if len(s1.Items) != 1 {
-		t.Errorf("len(s1)expeted:%s\nreceived:%s", 1, len(s1.Items))
+	s1 := new(SquareMatrix).Wrap(new(SquareMatrix).Wrap("test")).Flatten()
+	if len(s1) != 1 {
+		t.Errorf("len(s1)expeted:%s\nreceived:%s", 1, len(s1))
 	}
-	if s1.Items[0][0] != "test" {
-		t.Errorf("s1[0][0]\nexpeted:%s\nreceived:%s", "test", s1.Items[0][0])
+	if s1[0][0] != "test" {
+		t.Errorf("s1[0][0]\nexpeted:%s\nreceived:%s", "test", s1[0][0])
 	}
-	s2, _ = SquareMatrix{}.Flatten()
-	if len(s2.Items) != 0 {
-		t.Errorf("len(s2)expeted:%s\nreceived:%s", 1, len(s2.Items))
+	s2 := SquareMatrix{}.Flatten()
+	if len(s2) != 0 {
+		t.Errorf("len(s2)expeted:%s\nreceived:%s", 1, len(s2))
 	}
-	r3.From(Square{[]interface{}{}, []interface{}{}})
-	r3.Items[0] = append(r3.Items[0], SquareMatrix{})
-	s3, _ = r3.Flatten()
-	if len(s3.Items) != 0 {
-		t.Errorf("len(s2)expeted:%s\nreceived:%s", 0, len(s3.Items))
+	r3 := SquareMatrix{[]interface{}{}, []interface{}{}}
+	r3[0] = append(r3[0], SquareMatrix{})
+	s3 := r3.Flatten()
+	if len(s3) != 0 {
+		t.Errorf("len(s2)expeted:%s\nreceived:%s", 0, len(s3))
 	}
-	r4.From(Square{[]interface{}{3, 7}, []interface{}{2, 5}})
-	s4, _ = r4.Transform(func(v interface{}) interface{} {
+	r4 := SquareMatrix{[]interface{}{3, 7}, []interface{}{2, 5}}
+	s4 := r4.Transform(func(v interface{}) interface{} {
 		return new(SquareMatrix).Wrap(v)
 	})
-	s4, _ = s4.Flatten()
-	expected.From(Square{[]interface{}{3, 7}, []interface{}{2, 5}})
-	if reflect.DeepEqual(s4.Items, expected.Items) != true {
-		t.Errorf("DeepEqual\nexpeted: %s\nreceived:%s", expected.Items, s4.Items)
+	s4 = s4.Flatten()
+	expected = SquareMatrix{[]interface{}{3, 7}, []interface{}{2, 5}}
+	if reflect.DeepEqual(s4, expected) != true {
+		t.Errorf("DeepEqual\nexpeted: %s\nreceived:%s", expected, s4)
 	}
-	s5, _ = r4.Transform(func(v interface{}) interface{} {
+	s5 := r4.Transform(func(v interface{}) interface{} {
 		return s4
 	})
-	s5, _ = s5.Flatten()
-	expected.From(Square{
+	s5 = s5.Flatten()
+	expected = SquareMatrix{
 		[]interface{}{3, 7, 3, 7},
 		[]interface{}{2, 5, 2, 5},
 		[]interface{}{3, 7, 3, 7},
 		[]interface{}{2, 5, 2, 5},
-	})
-	if reflect.DeepEqual(s5.Items, expected.Items) != true {
-		t.Errorf("DeepEqual\nexpeted: %s\nreceived:%s", expected.Items, s5.Items)
+	}
+	if reflect.DeepEqual(s5, expected) != true {
+		t.Errorf("DeepEqual\nexpeted: %s\nreceived:%s", expected, s5)
 	}
 }
 
 func Test_SquareMatrix_Flatten_Broken(t *testing.T) {
-	var r, bad SquareMatrix
-	bad.From(Square{})
-	r.From(Square{
+	var bad SquareMatrix
+	r := SquareMatrix{
 		[]interface{}{
 			new(SquareMatrix).Wrap(1),
 			bad,
@@ -84,10 +81,11 @@ func Test_SquareMatrix_Flatten_Broken(t *testing.T) {
 			new(SquareMatrix).Wrap(1),
 			new(SquareMatrix).Wrap(1),
 		},
-	})
-	s, err := r.Flatten()
-	// t.Errorf("%s", s.Items)
+	}
+	s := r.Flatten()
+	err := s.IsOk()
+	// t.Errorf("%s", s)
 	if err == nil {
-		t.Error("SquareMatrix should not be well formed: %s", s.Items)
+		t.Error("SquareMatrix should not be well formed: %s", s)
 	}
 }
