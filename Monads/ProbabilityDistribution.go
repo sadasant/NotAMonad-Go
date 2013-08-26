a01a8b4ee3fe77761bd868af82aaf614aa2f3900
Daniel Rodriguez <sadasant@gmail.com>
2013-08-26 16:04:55 -0500

    ProbabilityDistribution wasn't a monad, now it is
    Monads are functions that returns themselves through transformations.

--- Monads/ProbabilityDistribution.go
+++ Monads/ProbabilityDistribution.go
@@ -6,26 +6,19 @@ import (
 
 type Distribution map[string]interface{}
 
-type ProbabilityDistribution struct {
-	Dict Distribution
-}
+// PD instead of ProbabilityDistribution
+type PD func() Distribution
 
-// Returns a probability distribution where the given value is the single
-// 100% likely possibility.
-func (p *ProbabilityDistribution) Wrap(k string) ProbabilityDistribution {
-	p.Dict = Distribution{k: 1.0}
-	return *p
-}
-
-func (p *ProbabilityDistribution) From(dict map[string]interface{}) error {
-	p.Dict = dict
-	return p.isWellFormed()
+func (p PD) From(dict Distribution) (PD, error) {
+	return func() Distribution {
+		return dict
+	}, dict.isWellFormed()
 }
 
 // well-formed distributions must add up to 100%:
-func (p ProbabilityDistribution) isWellFormed() error {
+func (d Distribution) isWellFormed() error {
 	var sum float64
-	for _, v := range p.Dict {
+	for _, v := range d {
 		if _v, ok := v.(List); ok {
 			sum += _v[1].(float64)
 		} else {
@@ -33,9 +26,16 @@ func (p ProbabilityDistribution) isWellFormed() error {
 		}
 	}
 	if sum != 1 {
-		return errors.New("The ProbabilityDistribution is not Well Formed")
+		return errors.New("The PD is not Well Formed")
 	}
 	return nil
+}
+
+// Returns a probability distribution where the given value is the single
+// 100% likely possibility.
+func (p PD) Wrap(k string) PD {
+	p, _ = p.From(Distribution{k: 1.0})
+	return p
 }
 
 // Returns a probability distribution over the result of drawing an input
@@ -43,21 +43,22 @@ func (p ProbabilityDistribution) isWellFormed() error {
 // When two distinct inputs are merged into the same output by the
 // transformation, the probability of the output is the sum of the
 // inputs' probabilities.
-func (p ProbabilityDistribution) Transform(t func(string) string) (r ProbabilityDistribution) {
-	r.Dict = map[string]interface{}{}
-	for k, v := range p.Dict {
+func (p PD) Transform(t func(string) string) (PD, error) {
+	pd := p()
+	rd := Distribution{}
+	for k, v := range pd {
 		trans := t(k)
-		if _, ok := r.Dict[trans]; ok {
-			r.Dict[trans] = r.Dict[trans].(float64) + v.(float64)
+		if _, ok := rd[trans]; ok {
+			rd[trans] = rd[trans].(float64) + v.(float64)
 		} else {
 			if _v, ok := v.(List); ok {
-				r.Dict[trans] = _v[1]
+				rd[trans] = _v[1]
 			} else {
-				r.Dict[trans] = v
+				rd[trans] = v
 			}
 		}
 	}
-	return
+	return p.From(rd)
 }
 
 // Returns a probability distribution over the result of drawing an
@@ -68,24 +69,25 @@ func (p ProbabilityDistribution) Transform(t func(string) string) (r Probability
 // The output probability of an item is its probability times the
 // probability of its distribution.  When an item appears in multiple
 // intermediate distributions, the corresponding probabilities are added.
-func (p ProbabilityDistribution) Flatten() (r ProbabilityDistribution) {
-	r.Dict = map[string]interface{}{}
-	for _, l := range p.Dict {
+func (p PD) Flatten() (PD, error) {
+	pd := p()
+	rd := Distribution{}
+	for _, l := range pd {
 		if _l, ok := l.(List); ok {
-			for k, v := range _l[0].(ProbabilityDistribution).Dict {
-				if _, ok := r.Dict[k]; ok {
-					r.Dict[k] = r.Dict[k].(float64) + (v.(float64) * _l[1].(float64))
+			for k, v := range _l[0].(PD)() {
+				if _, ok := rd[k]; ok {
+					rd[k] = rd[k].(float64) + (v.(float64) * _l[1].(float64))
 				} else {
 					if _v, ok := v.(List); ok {
-						r.Dict[k] = _v[1].(float64) * _l[1].(float64)
+						rd[k] = _v[1].(float64) * _l[1].(float64)
 					} else {
-						r.Dict[k] = v.(float64) * _l[1].(float64)
+						rd[k] = v.(float64) * _l[1].(float64)
 					}
 				}
 			}
 		} else {
-			return p
+			return p, nil
 		}
 	}
-	return
+	return p.From(rd)
 }
--- Monads/ProbabilityDistribution_test.go
+++ Monads/ProbabilityDistribution_test.go
@@ -6,65 +6,64 @@ import (
 	"testing"
 )
 
-func Test_ProbabilityDistribution_Wrap(t *testing.T) {
-	p := new(ProbabilityDistribution).Wrap("test")
+func Test_PD_Wrap(t *testing.T) {
+	p := new(PD).Wrap("test")
 	expected := Distribution{"test": 1.0}
-	if reflect.DeepEqual(p.Dict, expected) != true {
-		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, p.Dict)
+	if reflect.DeepEqual(p(), expected) != true {
+		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, p())
 	}
 }
 
-func Test_ProbabilityDistribution_Transform(t *testing.T) {
-	var p ProbabilityDistribution
-	err := p.From(Distribution{
+func Test_PD_Transform(t *testing.T) {
+	p, err := new(PD).From(Distribution{
 		"hey":    1.0 / 2,
 		"listen": 1.0 / 4,
 		"die":    1.0 / 4,
 	})
 	if err != nil {
-		t.Errorf("ProbabilityDistribution %s caused: %s", p.Dict, err)
+		t.Errorf("PD %s caused: %s", p(), err)
 	}
-	q := p.Transform(func(k string) string {
+	q, _ := p.Transform(func(k string) string {
 		return strconv.Itoa(len(k))
 	})
 	expected := Distribution{
 		"3": 3.0 / 4,
 		"6": 1.0 / 4,
 	}
-	if reflect.DeepEqual(q.Dict, expected) != true {
-		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, q.Dict)
+	if reflect.DeepEqual(q(), expected) != true {
+		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, q())
 	}
 }
 
-func Test_ProbabilityDistribution_Flatten(t *testing.T) {
-	p := new(ProbabilityDistribution).Wrap("test").Flatten()
+func Test_PD_Flatten(t *testing.T) {
+	p, _ := new(PD).Wrap("test").Flatten()
 	expected := Distribution{"test": 1.0}
-	if reflect.DeepEqual(p.Dict, expected) != true {
-		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, p.Dict)
+	if reflect.DeepEqual(p(), expected) != true {
+		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, p())
 	}
 
-	var a1, a2, s2 ProbabilityDistribution
-	a1.From(Distribution{
+	var pd PD
+	a1, _ := pd.From(Distribution{
 		"a":   1.0 / 7,
 		"bra": 1.0 / 7 * 2,
 		"ca":  1.0 / 7 * 4,
 	})
-	a2.From(Distribution{
+	a2, _ := pd.From(Distribution{
 		"da":  1.0 / 3,
 		"bra": 1.0 / 3 * 2,
 	})
-	s2.From(Distribution{
+	s2, _ := pd.From(Distribution{
 		"a1": List{a1, 1.0 / 5},
 		"a2": List{a2, 1.0 / 5 * 4},
 	})
-	s2 = s2.Flatten()
+	s2, _ = s2.Flatten()
 	expected = Distribution{
 		"a":   1.0 / 105 * 3,
 		"bra": 1.0 / 105 * 62,
 		"ca":  1.0 / 105 * 12,
 		"da":  1.0 / 105 * 28,
 	}
-	if reflect.DeepEqual(s2.Dict, expected) != true {
-		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, s2.Dict)
+	if reflect.DeepEqual(s2(), expected) != true {
+		t.Errorf("DeepEqual\nexpeted:%s\nreceived:%s", expected, s2())
 	}
 }
